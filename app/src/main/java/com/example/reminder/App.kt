1216d6a18ca658345a5111b8f4e8466b6f54a0bf
Erik Antonyan <erik.antonyan1994@gmail.com>
2022-04-21 08:40:52 +0400

    Add AppModule file

--- app/src/main/java/com/example/reminder/App.kt
+++ app/src/main/java/com/example/reminder/App.kt
@@ -1,6 +1,7 @@
 package com.example.reminder
 
 import android.app.Application
+import com.example.reminder.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -9,6 +10,7 @@ class App: Application() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
+            appModule
         }
     }
 }
---
+++ app/src/main/java/com/example/reminder/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.example.reminder.di
+
+import com.example.reminder.data.local.di.localModule
+
+val appModule = listOf(
+    localModule
+)
